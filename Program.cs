e16a84e81a5aa2174e71aa5e3e13a4713bc982b1
antjamespadgett <padgettanthonyj@gmail.com>
2022-01-14 15:27:23 -0600

    Completed exercise

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> videoGameNames = new List<string>
+            {"Doom","Call of Duty","Duke Nukem","NBALive" };
+
+            IEnumerable<string> longVideoGameNames =
+                videoGameNames.OrderBy(name => name.Length);
+
+            foreach (var i in videoGameNames)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
